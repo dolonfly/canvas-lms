1bffb4dd0df7ba0890bf4352d80491043e01ca31
dll <kaixin890312@163.com>
2023-12-21 17:25:57 +0800

    log

--- gems/live_events/lib/live_events/kafka/client.rb
+++ gems/live_events/lib/live_events/kafka/client.rb
@@ -49,13 +49,15 @@ module LiveEvents
 
     def initialize(config = nil, kafka_brokers_client = nil, kafka_broker_topic = nil, worker: nil)
       config ||= LiveEvents::ClientForKafka.config
-      @kafka_brokers_client = kafka_brokers_client || Kafka.new(config["kafka_seed_brokers"], client_id: "canvas-lms-application", resolve_seed_brokers: true).async_producer(
+      logger.info("-----kafka init ----->\n#{config.to_json}\n<---------")
+      @kafka_brokers_client = kafka_brokers_client || Kafka.new([config["kafka_seed_brokers"]], client_id: "canvas-lms-application").async_producer(
         # Trigger a delivery once 100 messages have been buffered.
         delivery_threshold: 100,
 
         # Trigger a delivery every 30 seconds.
         delivery_interval: 10,
       )
+      logger.info("-----kafka init end -----><---------")
       @kafka_broker_topic = kafka_broker_topic || config["kafka_broker_topic"]
       if worker
         @worker = worker
