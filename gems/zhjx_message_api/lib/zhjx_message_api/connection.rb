19f2847c36552557d4288b437d426e70dea59bc4
dll <kaixin890312@163.com>
2024-08-22 00:55:31 +0800

    f

--- gems/zhjx_message_api/lib/zhjx_message_api/connection.rb
+++ gems/zhjx_message_api/lib/zhjx_message_api/connection.rb
@@ -21,20 +21,13 @@ module ZhjxMessageApi
     # public (to gem)
     def send_direct_message(user_name, user_id, message)
 
-      logger.info("----------------send_direct_message(zhjx_message_api.connection.rb)--------------------------")
-      logger.info("user_name #{user_name}")
-      logger.info("user_id #{user_id}")
-      logger.info("message #{message}")
-      logger.info("message.to_s #{message.to_s}")
-      logger.info("================end send_direct_message(zhjx_message_api.connection.rb)==========================")
+      Rails.logger.info("----------------send_direct_message(zhjx_message_api.connection.rb)--------------------------")
+      Rails.logger.info("user_name #{user_name}")
+      Rails.logger.info("user_id #{user_id}")
+      Rails.logger.info("message #{message}")
+      Rails.logger.info("message.to_s #{message.to_s}")
+      Rails.logger.info("================end send_direct_message(zhjx_message_api.connection.rb)==========================")
     end
 
-    def self.logger
-      (@logger.respond_to?(:call) ? @logger.call : @logger) || default_logger
-    end
-
-    def self.default_logger
-      @_default_logger ||= Logger.new($stdout)
-    end
   end
 end
